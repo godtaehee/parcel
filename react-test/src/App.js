5d122b89c9a13f165dc16a546dba7a97f37f00ca
taehee <wonderfulhuman@naver.com>
2021-10-13 02:18:13 +0900

    간단한 App Test완료 fireEvent와 getByTestId는 공식문서에서 그렇게 추천하지않는 방법이라고 나와있다.

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -4,14 +4,34 @@ import { useState } from 'react';
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled] = useState(false);
 
   return (
     <div className="App">
       <header className="App-header">
         <h3 data-testid="counter">{counter}</h3>
         <div>
-          <button data-testid="minus-button">-</button>
-          <button data-testid="plus-button">+</button>
+          <button
+            disabled={disabled}
+            data-testid="minus-button"
+            onClick={() => setCounter((count) => count - 1)}
+          >
+            -
+          </button>
+          <button
+            disabled={disabled}
+            data-testid="plus-button"
+            onClick={() => setCounter((count) => count + 1)}
+          >
+            +
+          </button>
+          <button
+            data-testid="on/off-button"
+            style={{ background: 'blue' }}
+            onClick={() => setDisabled((disabled) => !disabled)}
+          >
+            on/off
+          </button>
         </div>
       </header>
     </div>
