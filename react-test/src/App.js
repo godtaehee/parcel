6f85e4c55d21fce7370d9344b5dcb40aaa2b42fb
taehee <wonderfulhuman@naver.com>
2021-10-13 01:37:45 +0900

    the counter starts at 0

---
+++ react-test/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3 data-testid="counter">{counter}</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
