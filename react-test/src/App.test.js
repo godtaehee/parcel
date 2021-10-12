06a2cdbf83d2659b40cce27e298359d55ec34aae
taehee <wonderfulhuman@naver.com>
2021-10-13 01:46:50 +0900

    Minus Button & Plus Button Test

--- react-test/src/App.test.js
+++ react-test/src/App.test.js
@@ -6,3 +6,15 @@ test('the counter starts at 0', () => {
   const counterElement = screen.getByTestId('counter');
   expect(counterElement).toHaveTextContent(0);
 });
+
+test('minus button has correct text', () => {
+  render(<App />);
+  const minusButtonElement = screen.getByTestId('minus-button');
+  expect(minusButtonElement).toHaveTextContent('-');
+});
+
+test('plus button has correct text', () => {
+  render(<App />);
+  const plusButtonElement = screen.getByTestId('plus-button');
+  expect(plusButtonElement).toHaveTextContent('+');
+});
