f34848b3f7542bfbd162560f9ab3ef24b06b2af2
taehee <wonderfulhuman@naver.com>
2021-10-14 23:25:35 +0900

    Class Component

---
+++ react-using-class/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    // 렌더 함수는 필수
+    const name = 'react';
+    return <div className="react">{name}</div>;
+  }
+}
