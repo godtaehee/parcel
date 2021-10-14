498bc83058b598de3bcde573c490ad49602365a8
taehee <wonderfulhuman@naver.com>
2021-10-14 23:30:04 +0900

    Default Props

--- react-using-class/src/App.js
+++ react-using-class/src/App.js
@@ -4,7 +4,7 @@ import MyComponent from './MyComponent';
 class App extends Component {
   render() {
     // 렌더 함수는 필수
-    return <MyComponent name="gd" />;
+    return <MyComponent />;
   }
 }
 export default App;
--- react-using-class/src/MyComponent.js
+++ react-using-class/src/MyComponent.js
@@ -3,5 +3,7 @@ import React from 'react';
 const MyComponent = ({ name }) => {
   return <div>{name}</div>;
 };
-
+MyComponent.defaultProps = {
+  name: '기본이름',
+};
 export default MyComponent;
